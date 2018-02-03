c853187ce2c44aceb75ef61431708ff4c118a470
azder <azhder@gmail.com>
2018-02-03 10:21:22 +0100

    streamline property access in X

--- expressionism/test-x.js
+++ expressionism/test-x.js
@@ -1,32 +1,44 @@
-const {
-    X$,
-    X$Object,
-    X$Null,
-    X$Array,
-    X$Function,
-} = require('./x');
+const X$ = require('./x');
 
 
 const log = (
-    o => console.log({
-        'keys':         o[X$._keys_],
-        '2str':         o[X$._2str_](o),
-        'call':         o[X$._call_](o), //o,
-        'call($.test)': o[X$._call_](X$.get(o, 'test')),
+    ($, n) => console.log({
+        [n + '.props']:         X$.mget($, X$._props_),
+        [n + '.2str()']:        X$.mget($, X$._2str_)($),
+        [n + '.call()']:        X$.mget($, X$._call_)($), //o,
+        [n + '.call($.a,$.b)']: X$.mget($, X$._call_)(X$.pget($, 'a'), X$.pget($, 'b')),
     })
 );
 
-
 [
-    [X$Object, Object.create(null)],
-    [X$Null, Object.create(null)],
-    [X$Array, Object.create(null)],
-    [X$Function, a => a + 2],
-].map(([X$Constructor, $]) => log(
-    X$Constructor($, {
-        key: 'test',
-        val: 1,
-    })
+    [X$.C(), X$.Obj, 'o'],
+    [X$.C(), X$.Nil, 'n'],
+    [X$.C(), X$.Arr, 'a'],
+    [(a, b) => a - 0 + b, X$.Fun, 'f'],
+].map(([$, C, name]) => log(
+    C(
+        $,
+        {
+            [X$._key_]: 'a',
+            [X$._val_]: 1,
+        },
+        {
+            [X$._key_]: 'b',
+            [X$._val_]: 2,
+        },
+    ),
+    name
 ));
 
+// const Person = X$.c(
+//     ($mutators, $, ...$$) => {
+//         console.log('CCC', $mutators, $, $$)
+//         $ = X$.Fun($);
+//         $mutators.pset($, 'a', 3);
+//         $mutators.pset($, 'b', 4);
+//         return $;
+//     }
+// );
+//
+// log(Person(Object.create(null)), 'person');
 
--- expressionism/x.js
+++ expressionism/x.js
@@ -5,25 +5,35 @@ const _false_ = Symbol('_false_');
 const _protos_ = Symbol('_protos_');
 const _call_ = Symbol('_call_');
 const _2str_ = Symbol('_2str_');
-const _keys_ = Symbol('_keys_');
+const _props_ = Symbol('_props_');
+const _metas_ = Symbol('_metas_');
 const _length_ = Symbol('_length_');
 
-// const X$call = Function.prototype.call;
-// const X$tos = Object.prototype.toString;
+const _key_ = Symbol('_key_');
+const _val_ = Symbol('_val_');
+
+
+const X$Create = (
+    () => Object.create(null)
+);
 
 const X$nil = (
     ($) => (null === $) || (void 0 === $)
 );
 
+const X$df = (
+    ($, v) => X$nil(v) ? $ : v
+);
+
 const X$null = (
-    ($) => $[_protos_].includes(X$Null)
+    ($) => $[_protos_].includes(X$Nil)
 );
 
 const X$ident = (
     ($) => ($)
 );
 
-const X$const = (
+const X$frozen = (
     ($) => Object.freeze({...$})
 );
 
@@ -35,142 +45,194 @@ const X$unshift = (
     ($, item) => [item, ...$]
 );
 
-const X$protadd = X$push;
+const X$padd = X$push;
 
 const X$first = (
     ($) => ($[0])
+);
+
+const X$second = (
+    ($) => ($[1])
 );
 
 const X$last = (
     ($) => ($[$.length - 1])
 );
 
-const X$set = (
-    ($, key, val) => {
-        X$push($[_keys_], key);
-        $[key] = val;
+const X$pset = (
+    ($, k, v) => {
+        X$mset($, _props_, X$push(X$mget($, _props_), k));
+        $[k] = v;
         return $;
     }
 );
 
-const X$get = (
-    ($, key) => {
+const X$pget = (
+    ($, k) => {
         // TODO: @azder: return X$Undefined when missing
-        return $[key];
+        return $[k];
     }
 );
 
-X$length = (
-    ($) => $[_keys_].length
+const X$mset = (
+    ($, k, v) => {
+        $[_metas_][k] = v;
+        return $;
+    }
+);
+
+const X$mget = (
+    ($, k) => {
+        // TODO: @azder: return X$Error when missing
+        return X$df(X$metas[k], $[_metas_][k]);
+    }
+);
+
+const X$length = (
+    ($) => $[_props_].length
 );
 
 const X$props2str = (
-    ($) => $[_keys_].map(key => ('' + key + ':' + $[key]))
+    ($) => X$mget($, _props_).map(k => ('' + k + ':' + $[k]))
 );
 
-const X$proto2str = (
-    ($) => X$last($[_protos_]).name //$[_protos_].map(p => p.name) //X$first($[_protos_]).name
+const X$props2arr = (
+    ($) => X$mget($, _props_).map(k => ({[k]: $[k]}))
 );
 
-const X$object2str = (
-    ($) => '' + X$proto2str($) + ':𝜔{' + X$props2str($) + '}𝜔'
-);
 
-const X$array2str = (
-    ($) => '' + X$proto2str($) + ':𝛼[' + X$props2str($) + ']𝛼'
-);
+const X$p2str = (
+    ($) => {
 
-const X$function2str = (
-    ($) => '' + X$proto2str($) + ':𝜆{' + $[_call_] + ',' + X$props2str($) + '}𝜆'
-);
+        const s = X$second(X$mget($, _protos_));
 
-const X$Object = ((() => {
-
-    return (
-
-        function X$Object($, ...$$) {
-
-            $[_protos_] = [X$Object];
-            $[_keys_] = $$.map(({key}) => key);
-
-            $[_2str_] = X$object2str;
-            $[_call_] = X$const; // X$ident;
-            $[_length_] = X$length;
-
-            $[_null_] = _false_;
-
-            $$.forEach(({key, val}) => X$set($, key, val));
-
-            return $;
-
+        if (s === X$Arr || s === X$Fun || s === X$Nil) {
+            return '';
         }
 
-    );
+        const l = X$last(X$mget($, _protos_));
 
-})());
+        return l === X$Obj ? '' : '' + l.name + ':';
 
-const X$Null = ((() => {
+    }
+);
 
-    return (
+const X$obj2str = (
+    ($) => '' + X$p2str($) + '𝜔{' + X$props2str($) + '}𝜔'
+);
 
-        function X$Null($, ...$$) {
+const X$arr2str = (
+    ($) => '' + X$p2str($) + '𝛼[' + X$props2arr($) + ']𝛼'
+);
 
-            $ = X$Object($, ...$$);
-            $[_protos_] = X$protadd($[_protos_], X$Null);
-            $[_null_] = _true_;
-            return $;
+const X$fun2str = (
+    ($) => '' + X$p2str($) + '𝜆{(' + X$mget($, _call_) + '),' + X$props2str($) + '}𝜆'
+);
 
-        }
-    );
-
-})());
-
-const X$Array = ((() => {
-
-    return (
-
-        function X$Array($, ...$$) {
-
-            $ = X$Object($, ...$$);
-            $[_protos_] = X$protadd($[_protos_], X$Array);
-            $[_2str_] = X$array2str;
-            return $;
-
-        }
-
-    );
-
-})());
-
-const X$Function = ((() => {
-
-    return (
-
-        function X$Function($, ...$$) {
-
-            $ = X$Object($, ...$$);
-            $[_protos_] = X$protadd($[_protos_], X$Function);
-            $[_2str_] = X$function2str;
-            $[_call_] = $;
-            return $;
-
-        }
-
-    );
-
-})());
+const X$nil2str = (
+    ($) => '' + X$p2str($) + '𝜈{' + X$props2str($) + '}𝜈'
+);
 
 
-module.exports = {
-    X$: {
-        get: X$get,
-        _protos_,
-        _keys_,
-        _2str_,
-        _call_,
-    },
-    X$Object,
-    X$Null,
-    X$Array,
-    X$Function,
+X$metas = {
+    [_2str_]:   X$obj2str,
+    [_call_]:   X$frozen,
+    [_length_]: X$length,
+    [_null_]:   _false_,
+    [_protos_]: []
 };
+
+const X$Obj = (
+
+    function Obj($, ...$$) {
+
+        $[_metas_] = [];
+
+        X$mset($, _props_, []);
+        X$mset($, _protos_, [Obj]);
+
+        return $$.reduce(
+            ($, {[_key_]: k, [_val_]: v}) => X$pset($, k, v),
+            $,
+        );
+
+    }
+
+);
+
+const X$Nil = (
+
+    function Nil($, ...$$) {
+
+        $ = X$Obj($, ...$$);
+
+        X$mset($, _protos_, X$padd(X$mget($, _protos_), Nil));
+        X$mset($, _2str_, X$nil2str);
+        X$mset($, _null_, _true_);
+
+        return $;
+
+    }
+);
+
+const X$Arr = (
+
+    function Arr($, ...$$) {
+
+        $ = X$Obj($, ...$$);
+
+        X$mset($, _protos_, X$padd(X$mget($, _protos_), Arr));
+        X$mset($, _2str_, X$arr2str);
+
+        return $;
+
+    }
+
+);
+
+const X$Fun = (
+
+    function Fun($, ...$$) {
+
+        $ = X$Obj($, ...$$);
+
+        X$mset($, _protos_, X$padd(X$mget($, _protos_), Fun));
+        X$mset($, _2str_, X$fun2str);
+
+        X$mset($, _call_, $);
+
+        return $;
+
+    }
+
+);
+
+
+const X$constructor = (
+    (C$) => ($, ...$$) => C$.call($, {pset: X$pset, mset: X$mset}, $, ...$$)
+);
+
+
+const X$ = X$Obj(
+    X$Create(),
+
+    {[_key_]: 'Obj', [_val_]: X$Obj},
+    {[_key_]: 'Nil', [_val_]: X$Nil},
+    {[_key_]: 'Arr', [_val_]: X$Arr},
+    {[_key_]: 'Fun', [_val_]: X$Fun},
+
+    {[_key_]: 'pget', [_val_]: X$pget},
+    {[_key_]: 'mget', [_val_]: X$mget},
+
+    {[_key_]: 'C', [_val_]: X$Create},
+    {[_key_]: 'c', [_val_]: X$constructor},
+
+    {[_key_]: '_protos_', [_val_]: _protos_},
+    {[_key_]: '_props_', [_val_]: _props_},
+    {[_key_]: '_2str_', [_val_]: _2str_},
+    {[_key_]: '_call_', [_val_]: _call_},
+    {[_key_]: '_key_', [_val_]: _key_},
+    {[_key_]: '_val_', [_val_]: _val_},
+);
+
+module.exports = X$;
